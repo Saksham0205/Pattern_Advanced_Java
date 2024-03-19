4a39877b980ade1e94cda816b2eb3a468577dff6
Saksham0205 <saksham252003@gmail.com>
2024-03-19 10:19:40 +0530

    Hollow Rectangle Pattern

--- src/Hollow_Rectangle.java
+++ src/Hollow_Rectangle.java
@@ -1,2 +1,22 @@
 public class Hollow_Rectangle {
+    public static void hollow(int rows,int columns){
+        for(int i=1;i<=rows;i++){
+            for(int j=1;j<=columns;j++)
+            {
+                if(i==1||i==rows||j==1||j==columns){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+            }
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        hollow(4,5);
+    }
 }
+
